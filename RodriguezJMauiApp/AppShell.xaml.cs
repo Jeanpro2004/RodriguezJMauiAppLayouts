3474b983a8e96c1aa37190166c929403b493bd96
laboratorio <laboratorio@UPOAULA10714>
2025-05-22 08:21:01 -0500

    Agregar archivos de proyecto.

---
+++ RodriguezJMauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RodriguezJMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RodriguezJMauiApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace RodriguezJMauiApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
